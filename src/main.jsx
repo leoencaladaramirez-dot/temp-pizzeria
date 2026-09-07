252f38438141f5ff8f74a9253616643362f2b81e
Marlon Encalada <leoencaladaramirez@gmail.com>
2026-09-07 01:25:59 -0300

    Fix: configurar base path y HashRouter para GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { CartProvider } from './context/CartContext';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom' 
+import App from './App.jsx'
+import CartProvider from './context/CartContext.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
-  </React.StrictMode>
-);
+    <HashRouter> 
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </HashRouter>
+  </React.StrictMode>,
+)
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/react-plugin' 
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/pizzeria-mamma-mia/', 
 })
