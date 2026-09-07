d0f0259818de9629f8c6f9cce6364533be5c354f
Marlon Encalada <leoencaladaramirez@gmail.com>
2026-09-07 01:57:24 -0300

    Fix: limpiar main.jsx para evitar doble router

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom' 
 import App from './App.jsx'
-import CartProvider from './context/CartContext.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter> 
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </HashRouter>
+    <App />
   </React.StrictMode>,
 )
